539ff91c9b0cab4d839ac7a74577b1e560964215
Richard Bramley <rabramley@gmail.com>
2018-09-15 07:54:26 +0100

    Refactor standard views

--- upload/__init__.py
+++ upload/__init__.py
@@ -1,13 +1,14 @@
-"""Aplication to manage Frostgrave wizards
+"""BRC Study Data Upload Site
 """
 import os
 import traceback
-from flask import Flask, render_template, send_from_directory
+from flask import Flask
+from flask_security import Security, SQLAlchemySessionUserDatastore
 from .ui import blueprint as ui_blueprint
 from upload.database import db
 from upload.admin import init_admin
 from upload.template_filters import init_template_filters
-
+from upload.standard_views import init_standard_views
 
 def create_app(config):
     """ Used to create flask application"""
@@ -20,28 +21,6 @@ def create_app(config):
     app.register_blueprint(ui_blueprint)
     init_admin(app)
     init_template_filters(app)
-
-    @app.route('/favicon.ico')
-    def favicon():
-        return send_from_directory(os.path.join(app.root_path, 'static'),
-            'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-    @app.errorhandler(404)
-    def missing_page(exception):
-        """Catch internal 404 errors, display
-            a nice error page and log the error.
-        """
-        print(traceback.format_exc())
-        return render_template('404.html'), 404
-
-    @app.errorhandler(500)
-    @app.errorhandler(Exception)
-    def internal_error(exception):
-        """Catch internal exceptions and 500 errors, display
-            a nice error page and log the error.
-        """
-        print(traceback.format_exc())
-        app.logger.error(traceback.format_exc())
-        return render_template('500.html'), 500
+    init_standard_views(app)
 
     return app
---
+++ upload/standard_views.py
@@ -0,0 +1,27 @@
+import os
+import traceback
+from flask import render_template, send_from_directory
+
+def init_standard_views(app):
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'),
+            'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+    @app.errorhandler(404)
+    def missing_page(exception):
+        """Catch internal 404 errors, display
+            a nice error page and log the error.
+        """
+        print(traceback.format_exc())
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    @app.errorhandler(Exception)
+    def internal_error(exception):
+        """Catch internal exceptions and 500 errors, display
+            a nice error page and log the error.
+        """
+        print(traceback.format_exc())
+        app.logger.error(traceback.format_exc())
+        return render_template('500.html'), 500
