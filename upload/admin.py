f8a4bfb175a64b1b2d578ecea4ac74089222048d
Richard Bramley <rabramley@gmail.com>
2018-09-15 12:32:51 +0100

    Only admins can edit stuff in admin

--- upload/admin.py
+++ upload/admin.py
@@ -1,16 +1,27 @@
 import flask_admin as admin
+from flask_security import current_user
+from flask import current_app
 from flask_admin.contrib.sqla import ModelView
 from upload.database import db
-from upload.model import Study, User
+from upload.model import Study, User, Role
 
-class StudyView(ModelView):
+
+class CustomView(ModelView):
+
+    def is_accessible(self):
+        return current_user.has_role(Role.ADMIN_ROLENAME)
+
+
+class StudyView(CustomView):
 
     form_excluded_columns = ['date_created',]
 
-class UserView(ModelView):
+
+class UserView(CustomView):
 
     column_exclude_list = ['password',]
     form_columns = ['email', 'first_name', 'last_name', 'active']
+
 
 def init_admin(app):
     flask_admin = admin.Admin(app, name='Leicester BRC data Upload', url='/admin/')
