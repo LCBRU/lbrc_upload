ac0abf0a2d7c5280607d5f0fbd653374b25dc01e
Richard Bramley <rabramley@gmail.com>
2018-10-01 15:11:33 +0100

    Add live manage script

---
+++ manage_live.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import os
+from migrate.versioning.shell import main
+
+if __name__ == '__main__':
+    main(repository='migrations', url=os.environ['DATABASE_URI'], debug='False')
