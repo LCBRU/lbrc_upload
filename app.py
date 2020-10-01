1ed44ad148fed64af8d979d415fb4048406befed
Richard Bramley <rabramley@gmail.com>
2020-10-01 16:14:10 +0100

    WSGI needs application

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from upload import create_app
 
-app = create_app()
+application = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    application.run(host="0.0.0.0", port=8000)
