56b0be611d71aa226b75905619a49a85fb9f744d
Richard Bramley <rabramley@gmail.com>
2020-11-25 11:44:59 +0000

    Correct import

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from migrate.versioning.shell import main
-from .config import Config
+from config import Config
 
 if __name__ == "__main__":
     main(repository="migrations", url=Config.SQLALCHEMY_DATABASE_URI, debug="True")
