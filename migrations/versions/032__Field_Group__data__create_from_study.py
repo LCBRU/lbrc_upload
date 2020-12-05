3a3dbf06c12835df1a816ec53664efa217fe623f
Richard Bramley <rabramley@gmail.com>
2020-12-05 12:30:42 +0000

    Not litterally homeless

--- migrations/versions/032__Field_Group__data__create_from_study.py
+++ migrations/versions/032__Field_Group__data__create_from_study.py
@@ -1,14 +1,21 @@
-from itertools import groupby
-from upload.model import Study, Field
+from upload.model import Study
 from sqlalchemy.orm.session import sessionmaker
 from lbrc_flask.forms.dynamic import FieldGroup
+from sqlalchemy import table, column, select, true
+from sqlalchemy import MetaData, Table, Column, Integer, String
 
+metadata = MetaData()
+
+fields = Table('fields', metadata,
+    Column('id', Integer , primary_key=True),
+    Column('study_id', Integer),
+    Column('field_group_id', Integer),
+)
 
 def upgrade(migrate_engine):
+    conn = migrate_engine.connect()
     Session = sessionmaker(bind=migrate_engine)
     session = Session()
-
-    fields = groupby(session.query(Field).order_by(Field.study_id).all(), lambda f: f.study_id)
 
     for s in session.query(Study).all():
         fg = FieldGroup(name=s.name)
@@ -17,10 +24,9 @@ def upgrade(migrate_engine):
         s.field_group = fg
         session.add(s)
 
-        for f in fields[s.id]:
-            f.field_group = fg
-            session.add(f)
-    
+        upd = fields.update().where(fields.c.study_id == s.id).values(field_group_id=fg.id)
+        conn.execute(upd)
+
     session.commit()
 
 
