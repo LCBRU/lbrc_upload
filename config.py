c94e7a37449ee88c16662fa168677f5ca2d25f46
Richard Bramley <rabramley@gmail.com>
2020-12-05 11:37:08 +0000

    For the love of God!

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
 from lbrc_flask.config import BaseConfig, BaseTestConfig
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 class Config(BaseConfig):
     FILE_UPLOAD_DIRECTORY = os.environ["FILE_UPLOAD_DIRECTORY"]
--- manage.py
+++ manage.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 from migrate.versioning.shell import main
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from config import Config
 
 if __name__ == "__main__":
