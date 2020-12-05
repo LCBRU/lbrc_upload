1107eb88e0e58c1bb4fe548f7324f0b4c53f0043
Richard Bramley <rabramley@gmail.com>
2020-12-05 11:56:42 +0000

    Of course, things have already changed

--- migrations/versions/032__Field_Group__data__create_from_study.py
+++ migrations/versions/032__Field_Group__data__create_from_study.py
@@ -1,4 +1,5 @@
-from upload.model import Study
+from itertools import groupby
+from upload.model import Study, Field
 from sqlalchemy.orm.session import sessionmaker
 from lbrc_flask.forms.dynamic import FieldGroup
 
@@ -7,17 +8,19 @@ def upgrade(migrate_engine):
     Session = sessionmaker(bind=migrate_engine)
     session = Session()
 
+    fields = groupby(session.query(Field).order_by(Field.study_id).all(), lambda f: f.study_id)
+
     for s in session.query(Study).all():
         fg = FieldGroup(name=s.name)
         session.add(fg)
 
-        for f in s.fields:
-            f.field_group = fg
-            session.add(f)
-    
         s.field_group = fg
         session.add(s)
 
+        for f in fields[s.id]:
+            f.field_group = fg
+            session.add(f)
+    
     session.commit()
 
 
